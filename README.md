58f9026b0e3a85d8ff27437c4bbf0ed66af91ff7
Jakub Zagórski <jaqobbdev@gmail.com>
2020-11-15 15:37:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Paid Activities
+![Workflow: build](https://github.com/jaqobb/paid-activities/workflows/build/badge.svg) [![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+
+Paid Activities is a Spigot plugin that lets you reward players when they do certain activities.
+
+Click [here]() to get to the project's SpigotMC page.
+
+### Features
+
+### Showcase
+
+### Usage
+
+### Requirements
---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    java
+    id("net.minecrell.plugin-yml.bukkit") version "0.3.0"
+    id("com.github.johnrengelman.shadow") version "5.2.0"
+}
+
+group = "dev.jaqobb"
+version = "1.0.0"
+description = "Spigot plugin that lets you reward players when they do certain activities"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+bukkit {
+    name = "PaidActivities"
+    main = "dev.jaqobb.paidactivities.PaidActivitiesPlugin"
+    version = project.version as String
+    depend = listOf("Vault")
+    description = project.description
+    author = "jaqobb"
+    website = "https://jaqobb.dev"
+}
+
+repositories {
+    jcenter()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") {
+        content {
+            includeGroup("org.spigotmc")
+        }
+    }
+    maven("https://oss.sonatype.org/content/repositories/snapshots/") {
+        content {
+            includeGroup("net.md-5")
+
+        }
+    }
+    maven("http://nexus.hc.to/content/repositories/pub_releases/") {
+        content {
+            includeGroup("net.milkbowl.vault")
+        }
+    }
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.16.4-R0.1-SNAPSHOT")
+    compileOnly("net.milkbowl.vault:VaultAPI:1.7")
+}
